868e82bf8026b31a82e5f534333d3bd12319d37b
Vu Son <sonvn@smartosc.com>
2020-11-12 17:31:51 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	stdHttp "net/http"
+
+	"github.com/ThreeDotsLabs/watermill"
+	"github.com/ThreeDotsLabs/watermill-http/pkg/http"
+	"github.com/ThreeDotsLabs/watermill/message"
+)
+
+func main() {
+	logger := watermill.NewStdLogger(true, true).With(
+		watermill.LogFields{},
+		)
+	httpSubscriber, err := http.NewSubscriber(
+		"localhost:9000",
+		http.SubscriberConfig{
+			Router: nil,
+			UnmarshalMessageFunc: func(topic string, request *stdHttp.Request) (*message.Message, error) {
+				var body []byte
+				request.Body.Read(body)
+				logger.Info("UnmarshalMessageFunc Invoked", nil)
+				return message.NewMessage(
+						watermill.NewUUID(),
+						body,
+					), nil
+			},
+		},
+		logger,
+		)
+	if err != nil {
+		logger.Error("Could not create HTTP subscriber", err, nil)
+	}
+
+	r, err := message.NewRouter(
+		message.RouterConfig{},
+		logger,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	r.AddHandler(
+		"http_to_kafka",
+		"/webhooks", // this is the URL of our API
+		httpSubscriber,
+		"webhooks", // this is the topic the message will be published to
+		nil,
+		func(msg *message.Message) ([]*message.Message, error) {
+			logger.Info("HandleFunc invoked", nil)
+			return []*message.Message{msg}, nil
+		},
+	)
+	// Run router asynchronously
+	go r.Run(context.Background())
+
+	// Check if router is running then start HTTP server
+	<-r.Running()
+	err = httpSubscriber.StartHTTPServer()
+	if err != nil {
+		logger.Error("Could not start HTTP server", err, nil)
+	}
+}
